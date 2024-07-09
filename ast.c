511bd5b91fb11c27aed40c7937233537edfacf14
StevenRaoul <sraoul@et.intechinfo.fr>
2024-07-09 14:47:33 +0200

    ast_new_variable

--- ast.c
+++ ast.c
@@ -4,13 +4,25 @@
 #include "ast.h"
 
 
-ast_t *ast_new_integer (long val) {
+ast_t *ast_new_integer(long val) {
   ast_t *intNode = (ast_t *)malloc(sizeof(ast_t));
-    if (intNode == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
-        exit(EXIT_FAILURE);
-    }
-    intNode->type = AST_INTEGER;
-    intNode->integer = val;
-    return intNode;
+  if (intNode == NULL) {
+    fprintf(stderr, "Erreur d'allocation mémoire\n");
+    exit(EXIT_FAILURE);
+  }
+  intNode->type = AST_INTEGER;
+  intNode->integer = val;
+  return intNode;
+}
+
+ast_t *ast_new_variable(char *name, var_type_e type) {
+  ast_t *varNode = (ast_t *)malloc(sizeof(ast_t));
+  if (varNode == NULL) {
+    fprintf(stderr, "Erreur d'allocation mémoire\n");
+    exit(EXIT_FAILURE);
+  }
+  varNode->type = AST_VARIABLE;
+  varNode->var.name = name;
+  varNode->var.type = type;
+  return varNode; 
 }
